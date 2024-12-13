90ed1d9de367b15f6203e7e246f2af477e2c93e9
karlo-kasi <karlokasi1995@gmail.com>
2024-12-13 13:33:05 +0100

    Add: small fixed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// inizio script barra durata canzone
 const progressBar = document.querySelector(".progress-bar");
 
 
@@ -17,3 +18,21 @@ window.addEventListener("load", () => {
     progressBar.value = initialValue; 
     updateProgressBar(initialValue); 
 });
+// Fine script barra durata canzone
+
+
+// inizio script barra volume
+const volumeBar = document.querySelector(".volume-bar");
+
+volumeBar.addEventListener("input", (e) => {
+    const value = e.target.value;
+   
+    volumeBar.style.background = `linear-gradient(to right, #5A5A5A ${value}%, #C1C1C1 ${value}%)`;
+});
+
+
+window.addEventListener("load", () => {
+    const initialValue = volumeBar.value;
+    volumeBar.style.background = `linear-gradient(to right, #5A5A5A ${initialValue}%, #C1C1C1 ${initialValue}%)`;
+});
+// Fine script barra volume
